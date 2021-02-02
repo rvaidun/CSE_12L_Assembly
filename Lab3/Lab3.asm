d927aa5d9263df6ba28857d052b674d3db8f1aba
rahulvaidun <rahul.vaidun@gmail.com>
2021-02-02 12:11:22 -0800

    Added pseudocode

--- Lab3/Lab3.asm
+++ Lab3/Lab3.asm
@@ -11,3 +11,16 @@
 #
 # Notes: This program is intended to be run from the MARS IDE.
 ##########################################################################
+#        												Pseudocode
+# infinite loop:
+# 	prompt user to enter height of pattern
+# 	if number greater than 0:
+# 		printTriangle
+# 		break the infinte loop
+#		else print error message
+# printTrinagle:
+# for x from 0 to height:
+# 	for y from 0 to (x1)*2-1:
+# 		if y equals x print x+1 and a tab
+#			else print a star and a tab
+# 	print a newline
