19bd684173e0c48fc3649b73f15679ce98236010
rahulvaidun <rahul.vaidun@gmail.com>
2021-02-02 12:00:20 -0800

    added header

---
+++ Lab3/Lab3.asm
@@ -0,0 +1,13 @@
+##########################################################################
+# Created by: Vaidun, Rahul
+# rvaidun
+# 2 February 2021
+#
+# Lab 3: ASCII-risks (Asterisks)
+# CSE12/L, Computer Systems and Assembly Language
+# UC Santa Cruz, Winter 2021
+#
+# Description: This program will print out a pattern with numbers and stars (asterisks).
+#
+# Notes: This program is intended to be run from the MARS IDE.
+##########################################################################
