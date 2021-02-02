fea06b98054e132022f62524a16349a5597055d8
rahulvaidun <rahul.vaidun@gmail.com>
2021-02-02 13:19:40 -0800

    error logic when user enters number less than or equal to 0

--- Lab3/Lab3.asm
+++ Lab3/Lab3.asm
@@ -24,3 +24,34 @@
 # 		if y equals x print x+1 and a tab
 #			else print a star and a tab
 # 	print a newline
+.data
+	prompt:		.asciiz "Enter the height of the pattern (must be greater than 0): "
+	star:		.asciiz "*\t"
+	newline:	.asciiz "\n"
+	tab:		.asciiz "\t"
+	errmsg:		.asciiz "Invalid entry!\n"
+
+.text
+main:
+	j ValidNum
+	
+	j Exit
+
+ValidNum:
+	la $a0, prompt			# $a0 = address of prompt
+	li $v0, 4				# preparation to call print_string()
+	syscall					# call print_string()
+	
+	li $v0, 5				# preparation to call read_int()	
+	syscall					# call read_int()
+	move $s0, $v0			# $s0 = height of pattern
+	
+	bgt $s0, $zero, Outer   # if $s0 > 0, go to Outer
+	la $a0, errmsg			# $a0 = address of errmsg
+	li $v0, 4				# preparation to call print_string()
+	syscall					# call print_string()
+	j ValidNum
+Outer:
+Exit:
+	li $v0, 10				# preparation to exit
+	syscall					# exit the program
