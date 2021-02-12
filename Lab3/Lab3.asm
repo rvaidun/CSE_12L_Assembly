13076ab3212820f0bd1362c7ee468236bc01ac5f
rahulvaidun <rahul.vaidun@gmail.com>
2021-02-12 01:22:49 -0800

    modify singular comment line on lab3

--- Lab3/Lab3.asm
+++ Lab3/Lab3.asm
@@ -65,7 +65,7 @@ printTriangle:
     ###################### inner loop #######################
         li $t1, 0                       # beginning of inner loop (y)
         addi $t2, $t0, 1                # add $t0 and $t1 and store in $t2
-        mul  $t2, $t2, 2                # multiply $t2 and $t2 and store in $t2
+        mul  $t2, $t2, 2                # multiply $t2 and 2 and store in $t2
         addi $t2, $t2, -1               # subtract 1 from $t2, $t2 is stopping point for inner loop
         start_inner_loop:
             beq $t1, $t2, end_inner_loop   # stop inner loop if $t1 = $t2
