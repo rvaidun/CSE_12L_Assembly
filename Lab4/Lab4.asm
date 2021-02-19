24d85e7e91eea65f430dbac0922ec3a827f75677
rahulvaidun <rahul.vaidun@gmail.com>
2021-02-18 23:23:02 -0800

    open and read file. when buffer is finished update buffer. when file ends exit

--- Lab4/Lab4.asm
+++ Lab4/Lab4.asm
@@ -36,6 +36,7 @@
     errorMisMatch2:     .asciiz " at index "
     errorStack:         .asciiz "ERROR - Brace(s) still on stack: "
     newline:            .asciiz "\n"
+    filebuffer:         .space 128
 .text
 main:
     la $a0, enterMsg
@@ -57,14 +58,54 @@ main:
     jal firstNumber
 
     jal validFileName
+
+    # Open File
+    move $a0, $s0      # move file name to arguments
+    li $a1, 0          # set mode to read
+    li $v0, 13         # prepare to open_file
+    syscall
+    move $s1, $v0      # save file descriptor to $s1, address of opened file
+    li $s2, 0          # $s2 amount of items in stack
+
+    jal readFile       # read file to buffer
+    li $t0, 0          # $t0 is the amount of times we looped through current buffer
+    move $t1, $v1      # $t2 is max amount of times we can loop
+    la $t2, filebuffer # load address of file buffer to $t2
+
+    loopbuffer:
+    beq $t0, $t1, readFile
+    lb $a0, 0($t2)
+    li $v0, 11
+    syscall
+    addi $t0, $t0, 1
+    addi $t2, $t2, 1
+    j loopbuffer
+
     j Exit
+
+############################################################################
+# Procedure: readFile
+# Description: read the file to buffer
+# registers to be used:
+#   $s1 - address of opened file
+#   $v1 - return amount of bytes in buffer
+############################################################################
+readFile:
+    move $a0, $s1
+    la $a1, filebuffer
+    li $a2, 128
+    li $v0, 14
+    syscall
+    beq $v0, 0, Exit
+    move $v1, $v0
+    jr $ra
 ############################################################################
 # Procedure: firstNumber
 # Description: Check if the first character is a number and if true then print error
 # registers to be used:
-#   $t0 is first character
-#   $t1, $t2, $t3 are used to check of number falls in the proper ascii range
-#   $ra to jump register to main
+#   $t0 - first character
+#   $t1, $t2 - Boolean to check if character falls between specific ascii range
+#   $t5 - AND of $t1, $t2
 ############################################################################
 firstNumber:
     # Check for uppsercase letters
@@ -91,15 +132,18 @@ firstNumber:
 # Description: Checks if each character is valid, a-z A-Z 0-9 . _ 
 # AND checks if length is greater than 20
 # registers to be used:
-#   $t0 is first character
-#   $t1, $t2, $t3 are used to check of number falls in the proper ascii range
-#   $ra to jump register to main
+#   $t0 - filename
+#   $t1 - counter for length of file
+#   $t2 - current character
+#   $t3, $t4 - Boolean to check if character falls specific between ascii range
+#   $t5 - AND of $t3, $t4
 ############################################################################
 validFileName:
     move $t0, $s0      # t0 is filename
     li $t1, 0          # t1 is counter for length of file
     validFileNameLoop:
         lb $t2, 0($t0) # t2 is current character
+        beqz $t2, return # if we are done with string return
         addi $t1, $t1, 1 # increment counter
         beq $t1, 20, printInvalidArg
         addi $t0, $t0, 1 # increment file name
@@ -138,9 +182,6 @@ validFileName:
         li $v0, 4
         syscall
         j Exit
-
-    validCharacter:
-    
 
 return:
     jr $ra
